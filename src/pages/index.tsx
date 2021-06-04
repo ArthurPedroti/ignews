9711fcbf3446dfe8a763a13f771500a27d700699
Arthur Pedroti <arthurpedroti@gmail.com>
2021-06-04 10:55:35 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import styles from '../styles/home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Hello World2</h1>
+    </>
+  )
+}
